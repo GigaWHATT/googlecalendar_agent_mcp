7ac6db4742053d5fedf0120aaef7f6491d4404ed
Etienne <guegan.etienne@gmail.com>
2025-07-29 11:15:00 +0200

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,13 @@
+"""Main module for g-calendar."""
+
+
+def main():
+    """Main function, prints a greeting."""
+    print("Hello from g-calendar!")
+    return None
+
+
+# -----------RUN-------------
+if __name__ == "__main__":
+    # Run the main function when the script is executed.
+    main()
